17507708f64a0111fc044fc96de27aefbf676e69
Nikunj Mangukiya <mang0088@algonquinlive.com>
2023-04-23 13:41:25 -0400

    create portfolio

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+(function ($) {
+  /* ----------------------------------------------------------- */
+  /*  FEATURED SLIDE (TYPED SLIDER)
+	/* ----------------------------------------------------------- */
+
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 20,
+    backSpeed: 20,
+    startDelay: 1000,
+    loop: true,
+    loopCount: Infinity,
+  });
+})(jQuery);
